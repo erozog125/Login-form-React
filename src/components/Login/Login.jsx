c515b2dbe2a1c4ad6384bea7cce7aa497dd5cf98
Edwin Rozo Gómez <erozog125@gmail.com>
2022-09-07 20:02:35 -0500

    Ref de login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React,{ useState, useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { BiShowAlt, BiHide } from 'react-icons/bi';
 
 // https://tailwindcss.com/docs/border-style
 
 export const Login = () => {
 
-  const handleLogin = (e) => {
-    e.preventDefault();    
-  }
+  const [showPass, setShowPass] = useState(false);
+  const [ typePass, setTypePass ] = useState('pass');
+  const pass = useRef(null);
+  const confirmPass = useRef(null);
 
+  let iconPass;
+  let type;
+  
+  
+    if (showPass) {
+      iconPass = <BiHide onClick={()=> setShowPass(!showPass)} className='show-pass' />;
+      type="text"
+    } else {
+      iconPass = <BiShowAlt onClick={()=> setShowPass(!showPass)} className='show-pass' />;
+      type="password"
+    } 
+
+    useEffect(() => {
+      if (pass === confirmPass) {
+        console.log('Funciona');
+      } else {
+        console.log('Funciona');
+      }
+    }, [pass, confirmPass])
+    
+    
   return (
     <div className='div-login'>
-      <BiShowAlt />
-      <BiHide />      
       <h2 className='font-bold text-cyan-500 text-7xl'>Login</h2>
-      <form className='form-login' action="" onSubmit={handleLogin}>
-        <label htmlFor="username">
-          <input className='input-form' type="text" id='username' name='username' placeholder="username" onChange={(e)=>setUserName(e.target.value)} />
-        </label>
-        <label htmlFor="password">
-          <div className="pass">
-            <input type="text" />
-          </div>
-        </label>        
-        <button className='btn-login'>Login</button>
+      <form className='form-login' action="" onSubmit={()=> console.log('Funciona')}>
+        <div className='div-pass' >
+          <input className='input-pass' ref={pass} type={type} id='password' name='password' placeholder="password" />
+          {iconPass}
+        </div>        
+        <div className='div-pass' >
+          <input className='input-pass' ref={confirmPass} type={type} id='password' name='password' placeholder="confirm password" />
+          {iconPass}
+        </div>        
       </form>
     </div>
   )
