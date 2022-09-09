cca1423239a0380232240ab10fbe4296854a62ac
Edwin Rozo Gómez <erozog125@gmail.com>
2022-09-08 19:33:21 -0500

    Validate password

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,4 @@
-import React,{ useState, useEffect, useRef } from 'react';
-import { useRef } from 'react';
+import React,{ useState, useEffect } from 'react';
 import { BiShowAlt, BiHide } from 'react-icons/bi';
 
 // https://tailwindcss.com/docs/border-style
@@ -7,43 +6,47 @@ import { BiShowAlt, BiHide } from 'react-icons/bi';
 export const Login = () => {
 
   const [showPass, setShowPass] = useState(false);
-  const [ typePass, setTypePass ] = useState('pass');
-  const pass = useRef(null);
-  const confirmPass = useRef(null);
-
+  const [ pass, setPass ] = useState('');
+  const [ confirmPass, setConfirmPass ] = useState('');
+  const [ equalPass, setEqualPass ] = useState("");
+    
   let iconPass;
   let type;
-  
-  
-    if (showPass) {
-      iconPass = <BiHide onClick={()=> setShowPass(!showPass)} className='show-pass' />;
-      type="text"
-    } else {
-      iconPass = <BiShowAlt onClick={()=> setShowPass(!showPass)} className='show-pass' />;
-      type="password"
-    } 
-
-    useEffect(() => {
-      if (pass === confirmPass) {
-        console.log('Funciona');
-      } else {
-        console.log('Funciona');
-      }
-    }, [pass, confirmPass])
     
+  if (showPass) {
+    iconPass = <BiHide onClick={()=> setShowPass(!showPass)} className='show-pass' />;
+    type="text"
+  } else {
+    iconPass = <BiShowAlt onClick={()=> setShowPass(!showPass)} className='show-pass' />;
+    type="password"
+  } 
+  
+  const getPass = event => setPass(event.target.value)
+  const getConfirmPass = event => setConfirmPass(event.target.value)
+  
+  useEffect(() => {
+    if (pass.length > 0 && confirmPass.length > 0) {
+      if (pass === confirmPass) {        
+        setEqualPass("Correct!")
+      } else {
+        setEqualPass("password not matches!")
+      }
+    }       
+    }, [pass,confirmPass])
     
   return (
     <div className='div-login'>
       <h2 className='font-bold text-cyan-500 text-7xl'>Login</h2>
       <form className='form-login' action="" onSubmit={()=> console.log('Funciona')}>
         <div className='div-pass' >
-          <input className='input-pass' ref={pass} type={type} id='password' name='password' placeholder="password" />
+          <input className='input-pass' onChange={getPass} type={type} id='password' name='password' placeholder="password" />
           {iconPass}
         </div>        
         <div className='div-pass' >
-          <input className='input-pass' ref={confirmPass} type={type} id='password' name='password' placeholder="confirm password" />
+          <input className='input-pass' onChange={getConfirmPass} type={type} id='confirm-password' name='password' placeholder="confirm password" />
           {iconPass}
-        </div>        
+        </div>
+        <span>{equalPass}</span>        
       </form>
     </div>
   )
